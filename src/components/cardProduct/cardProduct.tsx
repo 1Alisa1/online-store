eef26ea1ac9fb32a6417d90ea808e7a59afa0107
1Alisa1 <Alisa.dragun@mail.ru>
2023-01-06 22:38:39 +0300

    add animated card image spinner

--- src/components/cardProduct/cardProduct.tsx
+++ src/components/cardProduct/cardProduct.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import Spinner from '../spinner/spinner';
 import styles from './cardProduct.module.scss';
 
 interface CardProductProps {
@@ -6,18 +8,32 @@ interface CardProductProps {
   title: string;
 }
 
-const CardProduct: React.FC<CardProductProps> = ({img, price, title}) => {
+const CardProduct: React.FC<CardProductProps> = ({ img, price, title }) => {
+  const [loading, setLoading] = useState(true);
+
   if (title.length > 20) {
     title = title.substring(0, 20) + '...';
   }
 
   return (
     <div className={styles.card}>
-      <img src={img} alt="product" className={styles.cardImg}/>
+      <div className={styles.imgWrapper}>
+        <div style={{ display: loading ? 'block' : 'none' }}>
+          <Spinner />
+        </div>
+        <div style={{ display: loading ? 'none' : 'block' }}>
+          <img
+            src={img}
+            alt="product"
+            className={styles.cardImg}
+            onLoad={() => setLoading(false)}
+          />
+        </div>
+      </div>
       <div className={styles.cardPrice}>{price} $</div>
       <div className={styles.cardTitle}>{title}</div>
     </div>
   );
-}
+};
 
 export default CardProduct;
